8bf33d2c3abcf226de4bdbef7c42a16b038f004a
Snehasish Dawn <snehasishdawn29@gmail.com>
2026-04-01 23:34:23 +0530

    feat: implement sidebar component with context and mobile support

---
+++ apps/web/src/components/app-sidebar.tsx
@@ -0,0 +1,56 @@
+import { Edit, Plus } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Button } from "./ui/button"
+
+const items = [{ title: "New Thread", url: "/", icon: Edit }]
+
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton
+                    render={<a href={item.url} />}
+                    tooltip={item.title}
+                  >
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <div className="flex items-center justify-between">
+            <SidebarGroupLabel>Workspace</SidebarGroupLabel>
+            <Button variant="ghost" size="icon">
+              <Plus />
+            </Button>
+          </div>
+          <SidebarGroupContent>
+            <SidebarMenuItem>
+              <div className="my-2 text-center text-muted-foreground">
+                No workspaces available
+              </div>
+            </SidebarMenuItem>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,17 +1,21 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router"
+import { createRootRoute, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
 
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { TooltipProvider } from "@/components/ui/tooltip"
+
 const RootLayout = () => (
-  <>
-    <div className="flex gap-2 p-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>
-    </div>
-    <hr />
-    <Outlet />
+  <TooltipProvider>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-12 shrink-0 items-center gap-2 px-4"></header>
+        <Outlet />
+      </SidebarInset>
+    </SidebarProvider>
     <TanStackRouterDevtools />
-  </>
+  </TooltipProvider>
 )
 
 export const Route = createRootRoute({ component: RootLayout })
